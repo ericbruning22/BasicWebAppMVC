9e0b9174adde23ca604bb7f036f6fad0434df5bc
bruniet <eric.bruning@dillards.com>
2021-07-22 11:56:56 -0500

    add max length validation

--- BasicWebAppMVC/Models/User.cs
+++ BasicWebAppMVC/Models/User.cs
@@ -11,17 +11,21 @@ namespace BasicWebAppMVC.Models
         public int Id { get; set; }
 
         [RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage = "Invalid Name (Letters and Spaces only)")]
+        [MaxLength(100)]
         [Required(ErrorMessage = "Please enter Name")]
         public string Name { get; set; }
 
         [RegularExpression(@"^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+[.])+[a-z]{2,5}$", ErrorMessage = "Invalid Email")]
+        [MaxLength(50)]
         [Required(ErrorMessage = "Please enter Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Please enter Job Title")]
+        [MaxLength(50)]
         public string JobTitle { get; set; }
 
         [RegularExpression(@"^([A-Za-z]+(?: [A-Za-z]+)*),? ([A-Za-z]{2})$", ErrorMessage = "Invalid Location (ex: Little Rock, AR)")]
+        [MaxLength(75)]
         [Required(ErrorMessage = "Please enter Location")]
         public string Location { get; set; }
 
